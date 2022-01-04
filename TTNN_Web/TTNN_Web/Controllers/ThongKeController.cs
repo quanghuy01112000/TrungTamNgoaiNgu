bf6c7fae432b5a89ce74dcf9e7a85ec982fe97e3
Huy Nguyen <huy.nq@bliss-interactive.com>
2022-01-04 19:06:54 +0700

    add view basic to web

---
+++ TTNN_Web/TTNN_Web/Controllers/ThongKeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TTNN_Web.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        // GET: ThongKe
+        public ActionResult ThongKeA2()
+        {
+            return View();
+        }
+
+        public ActionResult ThongKeB1()
+        {
+            return View();
+        }
+    }
+}
